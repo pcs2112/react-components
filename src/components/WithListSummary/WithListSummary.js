7418583388390724607f0c53b17374680195d278
Vincent Chavez <vchavez92780@gmail.com>
2017-08-25 16:23:36 -0400

    Refactored the WithListSummary and made it into a function component

--- src/components/WithListSummary/WithListSummary.js
+++ src/components/WithListSummary/WithListSummary.js
@@ -2,78 +2,64 @@
  * React component to display the pagination summary
  * of a list of items.
  */
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { replaceArray, getDisplayName } from '../../utils';
 
 export const withListSummary = (WrappedComponent) => {
-  class WithListSummary extends Component {
-    static propTypes = {
-      currentPage: PropTypes.number.isRequired,
-      totalPages: PropTypes.number.isRequired,
-      totalItems: PropTypes.number.isRequired,
-      pageSize: PropTypes.number.isRequired,
-      currentPageTotalItems: PropTypes.number.isRequired,
-      summaryText: PropTypes.string,
-      paginationEnabled: PropTypes.bool
-    };
+  const WithListSummary = ({
+                             currentPage,
+                             totalPages,
+                             totalItems,
+                             pageSize,
+                             currentPageTotalItems,
+                             summaryText = null,
+                             paginationEnabled = false
+                           }) => {
+    let start = currentPage * pageSize + 1;
+    let end = start + currentPageTotalItems - 1;
 
-    static defaultProps = {
-      summaryText: null,
-      paginationEnabled: false
-    };
-
-    getSummary() {
-      const {
-        currentPage,
-        totalPages,
-        totalItems,
-        pageSize,
-        currentPageTotalItems,
-        summaryText,
-        paginationEnabled,
-      } = this.props;
-
-      let start = currentPage * pageSize + 1;
-      let end = start + currentPageTotalItems - 1;
-
-      if (end > totalItems) {
-        end = totalItems;
-        start = end - currentPageTotalItems + 1;
-      }
-
-      let finalSummaryText = summaryText;
-
-      if (paginationEnabled) {
-        if (summaryText === null) {
-          finalSummaryText = 'Displaying {start}-{end} of {count} result' + (currentPageTotalItems === 1 ? '' : 's') + '.';
-        }
-
-        const search = ['{start}', '{end}', '{count}', '{page}', '{pages}'];
-        const replace = [start, end, totalItems, currentPage + 1, totalPages];
-        finalSummaryText = replaceArray(finalSummaryText, search, replace);
-      } else {
-        if (finalSummaryText === null) {
-          finalSummaryText = 'Total {count} result' + (currentPageTotalItems === 1 ? '' : 's') + '.';
-        }
-
-        const search = ['{count}', '{start}', '{end}', '{page}', '{pages}'];
-        const replace = [currentPageTotalItems, 1, currentPageTotalItems, 1, 1];
-        finalSummaryText = replaceArray(finalSummaryText, search, replace);
-      }
-
-      return finalSummaryText;
+    if (end > totalItems) {
+      end = totalItems;
+      start = end - currentPageTotalItems + 1;
     }
 
-    render() {
-      return (
-        <WrappedComponent
-          {...this.props}
-          summaryText={this.getSummary()}
-        />
-      );
+    let finalSummaryText = summaryText;
+
+    if (paginationEnabled) {
+      if (summaryText === null) {
+        finalSummaryText = 'Displaying {start}-{end} of {count} result' + (currentPageTotalItems === 1 ? '' : 's') + '.';
+      }
+
+      const search = ['{start}', '{end}', '{count}', '{page}', '{pages}'];
+      const replace = [start, end, totalItems, currentPage + 1, totalPages];
+      finalSummaryText = replaceArray(finalSummaryText, search, replace);
+    } else {
+      if (finalSummaryText === null) {
+        finalSummaryText = 'Total {count} result' + (currentPageTotalItems === 1 ? '' : 's') + '.';
+      }
+
+      const search = ['{count}', '{start}', '{end}', '{page}', '{pages}'];
+      const replace = [currentPageTotalItems, 1, currentPageTotalItems, 1, 1];
+      finalSummaryText = replaceArray(finalSummaryText, search, replace);
     }
-  }
+
+    return (
+      <WrappedComponent
+        summaryText={finalSummaryText}
+      />
+    );
+  };
+
+  WithListSummary.propTypes = {
+    currentPage: PropTypes.number.isRequired,
+    totalPages: PropTypes.number.isRequired,
+    totalItems: PropTypes.number.isRequired,
+    pageSize: PropTypes.number.isRequired,
+    currentPageTotalItems: PropTypes.number.isRequired,
+    summaryText: PropTypes.string,
+    paginationEnabled: PropTypes.bool
+  };
 
   WithListSummary.displayName = `WithListSummary(${getDisplayName(WrappedComponent)})`;
 
