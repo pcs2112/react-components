07910b95e28c16fc17597e65fe883e7fb7ddeb71
Vincent Chavez <vchavez92780@gmail.com>
2017-08-25 21:49:40 -0400

    Added withPagination to the main export

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export withRoleAwareness from './components/WithRoleAwareness';
 export withListSummary from './components/WithListSummary';
 export withFormError from './components/WithFormError';
 export withErrorSummary from './components/WithErrorSummary';
+export withPagination from './components/WithPagination/WithPagination';
