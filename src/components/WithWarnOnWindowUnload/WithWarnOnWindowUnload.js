453f3a0415d6f03cd9030085c03099287814a359
Vincent Chavez <vchavez92780@gmail.com>
2017-09-25 10:49:33 -0400

    Updated router confirmation component

--- src/components/WithWarnOnWindowUnload/WithWarnOnWindowUnload.js
+++ src/components/WithWarnOnWindowUnload/WithWarnOnWindowUnload.js
@@ -5,6 +5,11 @@ import { getDisplayName } from 'javascript-utils/lib/react';
 
 export const withWarnOnWindowUnload = (WrappedComponent, leaveMessage = `Leave with unsaved changes?`) => {
   class WithWarnOnWindowUnload extends Component {
+    constructor(props) {
+      super(props);
+      this.routerWillLeave = this.routerWillLeave.bind(this);
+    }
+
     componentDidMount() {
       this.props.router.setRouteLeaveHook(this.props.route, this.routerWillLeave);
     }
