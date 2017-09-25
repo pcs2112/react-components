59aeed377267e03789fbf7bd8b0814c04e9e20c7
Vincent Chavez <vchavez92780@gmail.com>
2017-09-25 10:42:51 -0400

    Updated router confirmation component

--- src/components/WithWarnOnWindowUnload/WithWarnOnWindowUnload.js
+++ src/components/WithWarnOnWindowUnload/WithWarnOnWindowUnload.js
@@ -5,28 +5,13 @@ import { getDisplayName } from 'javascript-utils/lib/react';
 
 export const withWarnOnWindowUnload = (WrappedComponent, leaveMessage = `Leave with unsaved changes?`) => {
   class WithWarnOnWindowUnload extends Component {
-    componentDidUpdate() {
-      this._promptUnsavedChange(this.props.isUnSafeToUnload);
+    componentDidMount() {
+      this.props.router.setRouteLeaveHook(this.props.route, this.routerWillLeave);
     }
 
-    componentWillUnmount() {
-      window.onbeforeunload = null;
-    }
-
-    _promptUnsavedChange(isUnSafeToUnload = false) {
-      // Detecting page transition (prevent leaving by setting true)
-      // for React Router version > 2.4
-      if (isUnSafeToUnload) {
-        this.props.router.setRouteLeaveHook(this.props.route, () => isUnSafeToUnload && confirm(leaveMessage));
-      } else {
-        this.props.router.setRouteLeaveHook(this.props.route, () => {});
-      }
-
-      // Detecting browser close
-      if (isUnSafeToUnload) {
-        window.onbeforeunload = isUnSafeToUnload && (() => leaveMessage);
-      } else {
-        window.onbeforeunload = null;
+    routerWillLeave() {
+      if (this.props.isUnSafeToUnload) {
+        return leaveMessage;
       }
     }
 
